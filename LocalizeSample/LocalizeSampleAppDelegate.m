183d8e689b078e9ce7d83b9caaed98c915de0161
Masayuki Sakura <webmaster@tool-lab.com>
2019-03-24 12:22:51 +0900

    first commit

---
+++ LocalizeSample/LocalizeSampleAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  LocalizeSampleAppDelegate.m
+//  LocalizeSample
+//
+//  Created by Tool Labs on 2013/04/14.
+//  Copyright 2013 Tool Labs.
+//
+
+#import "LocalizeSampleAppDelegate.h"
+
+@implementation LocalizeSampleAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+    //[messageString setStringValue:@"Application launched!"];
+    [messageString setStringValue:NSLocalizedString(@"STARTUP_TEXT", @"アプリ起動完了メッセージ")];
+}
+
+@end
